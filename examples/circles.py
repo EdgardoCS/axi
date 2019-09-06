8388598ceea64a7bf26237d95e9d61e09b89ed94
EdgardoCS <edgardo_ches@hotmail.com>
2019-09-06 12:05:13 -0400

    circle changes

--- examples/circles.py
+++ examples/circles.py
@@ -1,6 +1,7 @@
-import axi
 import math
 import random
+import axi
+
 
 def circle(cx, cy, r, n):
     points = []
@@ -10,6 +11,7 @@ def circle(cx, cy, r, n):
         y = cy + math.sin(a) * r
         points.append((x, y))
     return points
+
 
 def random_points_on_circle(cx, cy, r, n):
     result = []
@@ -22,21 +24,23 @@ def random_points_on_circle(cx, cy, r, n):
         a += da
     return result
 
+
 def add(x, y, r, paths):
     if r < 1:
         return
     paths.append(circle(x, y, r, 90))
-    points = random_points_on_circle(x, y, r, 2)
+    points = random_points_on_circle(x, y, r, 1)
     for x, y in points:
         add(x, y, r / 2, paths)
 
+
 def main():
     paths = []
-    add(0, 0, 64, paths)
-    drawing = axi.Drawing(paths).rotate_and_scale_to_fit(11, 8.5).sort_paths()
-    im = drawing.render()
-    im.write_to_png('out.png')
+    add(0, 0, 2, paths)
+    new_path = [paths.pop()]
+    drawing = axi.Drawing(new_path).rotate_and_scale_to_fit(2, 2)
     axi.draw(drawing)
+
 
 if __name__ == '__main__':
     main()
